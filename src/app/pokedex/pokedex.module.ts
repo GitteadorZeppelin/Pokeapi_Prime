f94f5f2a8950f187adaf44b77815b8ec7b2acaa5
NPCJorge <practicas1nitropc@gmail.com>
2021-06-16 15:04:11 +0200

    Add routerlink

--- src/app/pokedex/pokedex.module.ts
+++ src/app/pokedex/pokedex.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokedexComponent } from './pokedex.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { PokedexComponent } from './pokedex.component';
     PokedexComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class PokedexModule { }
