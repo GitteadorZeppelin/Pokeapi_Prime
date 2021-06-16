d955cd76c6f14addf40c819503fb0ed8764301a5
NPCJorge <practicas1nitropc@gmail.com>
2021-06-16 15:03:55 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PokedexComponent
+  },{
+    path: 'pokemon/:num',
+    component: PokemonComponent
   },{
     path: '**',
     component: PokedexComponent
