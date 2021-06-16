de864b3ced23c610e9eb524f2543709ed2015184
NPCJorge <practicas1nitropc@gmail.com>
2021-06-16 15:03:46 +0200

    Add modlules + routerlink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 import { PokedexModule } from './pokedex/pokedex.module';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { ApiKachuService } from './Servicios/api-kachu.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    PokedexModule
+    PokedexModule,
+    PokemonModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [ApiKachuService, PokedexComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
