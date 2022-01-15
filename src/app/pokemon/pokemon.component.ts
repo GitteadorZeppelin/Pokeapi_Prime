1aee9a7b2998de7e35c7a7ebc14cac0460832be9
gitteadorzeppelin <jorgeallozags@gmail.com>
2022-01-15 17:21:32 +0100

    LOs mokempon

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApikachuService } from '../apikachu.service';
 
 @Component({
   selector: 'app-pokemon',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonComponent implements OnInit {
 
-  constructor() { }
+  pkmn: any;
+  urlimg: string;
+  constructor(
+    private apiKachu: ApikachuService,
+    private route: ActivatedRoute
+  ) { 
+
+    this.pkmn = "";
+    this.urlimg = "";
+  }
 
   ngOnInit(): void {
+    this.getPkmn();
+  }
+
+  getPkmn() {
+    let num: any;
+    this.route.params.subscribe(params => {
+      num = params.num;
+    });
+    this.apiKachu.getPKMN(num).then(datos => {
+      this.pkmn = datos;
+      console.log(datos);
+    })
+  }
+
+  creoClase(tipo1: any, tipo2: any){
+    if(tipo2 == undefined){
+      return tipo1+"-top " + tipo1+"-left " + tipo1+"-right " + tipo1+"-bottom"
+    } else {
+      return tipo1+"-top " + tipo1+"-left " + tipo2.type.name+"-right " + tipo2.type.name+"-bottom"
+    }
+  }
+
+  creoClaseTipo(tipo1: any){
+      return tipo1+"-top " + tipo1+"-left " + tipo1+"-right " + tipo1+"-bottom"
   }
 
 }
