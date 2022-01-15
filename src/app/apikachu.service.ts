c657fa3f21499d0b2e6e467889204ed8a24628c2
gitteadorzeppelin <jorgeallozags@gmail.com>
2022-01-15 17:11:09 +0100

    Api Request

--- src/app/apikachu.service.ts
+++ src/app/apikachu.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApikachuService {
+  url = "https://pokeapi.co/api/v2/pokemon?limit=898&offset=0"
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  getPKMN(id: number): Promise<any>{
+    return this.http.get("https://pokeapi.co/api/v2/pokemon/" + id + "/").toPromise()
+  }
+
 }
