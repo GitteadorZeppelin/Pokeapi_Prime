c708172c4b40067435cf9639e461d7d6e816c3f8
gitteadorzeppelin <jorgeallozags@gmail.com>
2022-01-15 17:11:21 +0100

    Template

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApikachuService } from '../apikachu.service';
 
 @Component({
   selector: 'app-pokedex',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokedexComponent implements OnInit {
 
-  constructor() { }
+  listaPkmn: any;
+  page: any;
+  constructor(
+    private apiKachu: ApikachuService
+  ) {
+    this.listaPkmn = []
+   }
 
   ngOnInit(): void {
+    this.page = 1;
+    this.getPkmn(this.page);
   }
 
+
+  async getPkmn(page: any) {
+    this.listaPkmn = [];
+    let contador = 1;
+    console.log(page)
+    for (let x = ((page-1)*12)+1; x <= (page*12); x++) {
+      await this.apiKachu.getPKMN(x).then(datos => {
+        this.listaPkmn[contador-1] = datos
+        contador++
+      }) 
+    }    
+  }
+
+  creoClase(tipo1 :any, tipo2:any){
+    if(tipo2 == undefined){
+      return tipo1+"-top " + tipo1+"-left " + tipo1+"-right " + tipo1+"-bottom"
+    } else {
+      return tipo1+"-top " + tipo1+"-left " + tipo2.type.name+"-right " + tipo2.type.name+"-bottom"
+    }
+  }
+
+  adelante(){
+    this.page++;
+    console.log(this.page)
+    this.getPkmn(this.page);
+  }
+  atras(){
+    this.page--;
+    this.getPkmn(this.page);
+  }
 }
